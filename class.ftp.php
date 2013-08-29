dc891b0f05b142cced954f66547ac7e92b767337
Andr3as <andr3as@me>
2013-08-29 23:15:08 +0200

    Fixes #2 and adds displaying of hidden files

--- class.ftp.php
+++ class.ftp.php
@@ -42,7 +42,7 @@
                 if (ftp_chdir($id, $path) === false) {
                     $this->getError("Impossible to Change Directory");
                 } else {
-                    $raw    = ftp_rawlist($id, ".");
+                    $raw    = ftp_rawlist($id, "-al .");
                     $parsed = $this->parseRawList($raw);
                     //Correct style
                     $dirs   = array();
