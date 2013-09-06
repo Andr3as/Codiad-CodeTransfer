e50dc9cf016bfb19cfb35222e8f165b3d8be42f4
Andr3as <andr3as@me>
2013-09-06 17:37:28 +0200

    Testing of absolute path corrected

--- class.transfer.php
+++ class.transfer.php
@@ -183,7 +183,7 @@
         }
         
         static public function getWorkspacePath($path) {
-            if (strpos($path, "/") == 0) {
+            if (strpos($path, "/") === 0) {
                 //Unix absolute path
                 return $path;
             }
